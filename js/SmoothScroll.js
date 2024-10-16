cfce3bd01c5bb76329dcba64e8c3f4a9dae7ce24
Amer Tahri-Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2024-10-16 13:26:22 +0100

    Update SmoothScroll.js

--- js/SmoothScroll.js
+++ js/SmoothScroll.js
@@ -21,7 +21,7 @@ var defaultOptions = {
     // Scrolling Core
     frameRate        : 150, // [Hz]
     animationTime    : 600, // [ms]
-    stepSize         : 100, // [px]
+    stepSize         : 120, // [px]
 
     // Pulse (less tweakable)
     // ratio of "tail" to "acceleration"
