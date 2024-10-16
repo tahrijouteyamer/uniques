211e5c84695d237cde455fcef743743bcace65d0
Amer Tahri-Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2024-10-16 13:17:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -89,6 +89,31 @@
 
   };
 
+  var initQuantitySpinner = function(){
+
+    $('.genre-qty').each(function(){
+
+      var $el_product = $(this);
+      var quantity = 0;
+
+      $el_product.find('.quantity-right-plus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          $el_product.find('#quantity').val(quantity + 1);
+      });
+
+      $el_product.find('.quantity-left-minus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          if(quantity>0){
+            $el_product.find('#quantity').val(quantity - 1);
+          }
+      });
+
+    });
+
+  }
+
   var initJarallax = function () {
     jarallax(document.querySelectorAll(".jarallax"));
 
